a0f2d342c571f5c7e0a8d0ffa8ea0c040baeae89
zhouzj <2473047707@qq.com>
2018-07-17 19:32:50 -0700

    Update star_polemo.sql

--- star_polemo.sql
+++ star_polemo.sql
@@ -3,15 +3,15 @@
 
  Source Server         : mysql
  Source Server Type    : MySQL
- Source Server Version : 80011
+ Source Server Version : 50716
  Source Host           : localhost:3306
  Source Schema         : star_polemo
 
  Target Server Type    : MySQL
- Target Server Version : 80011
+ Target Server Version : 50716
  File Encoding         : 65001
 
- Date: 17/07/2018 15:29:38
+ Date: 18/07/2018 10:28:58
 */
 
 SET NAMES utf8;
@@ -22,23 +22,23 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `t_activity`;
 CREATE TABLE `t_activity`  (
-  `id` int(11) NOT NULL,
-  `activity_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '活动名称',
-  `activity_address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '活动地址',
-  `activity_icon` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '活动头像',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '活动名称',
+  `address` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '活动地址',
+  `icon` varchar(25) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '活动头像',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '活动表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for t_contact_us
+-- Table structure for t_contact
 -- ----------------------------
-DROP TABLE IF EXISTS `t_contact_us`;
-CREATE TABLE `t_contact_us`  (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `t_contact`;
+CREATE TABLE `t_contact`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL DEFAULT -1 COMMENT '用户id',
-  `message_body` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '消息内容',
+  `message_body` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '发送消息内容',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '联系我们表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_game
@@ -46,12 +46,12 @@ CREATE TABLE `t_contact_us`  (
 DROP TABLE IF EXISTS `t_game`;
 CREATE TABLE `t_game`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `game_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '游戏名称',
-  `game_icon` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '游戏头像',
-  `game_details` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '游戏详情',
-  `game_address` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '游戏地址',
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '游戏名称',
+  `icon` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '游戏头像',
+  `details` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '游戏详情',
+  `address` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '游戏地址',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 10000 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '游戏表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_gift
@@ -59,36 +59,25 @@ CREATE TABLE `t_game`  (
 DROP TABLE IF EXISTS `t_gift`;
 CREATE TABLE `t_gift`  (
   `id` int(11) NOT NULL,
-  `gift_name` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼包名称',
-  `gift_details` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼包详情',
-  `gift_icon` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼包头像',
-  `gift_number` int(10) NOT NULL DEFAULT 0 COMMENT '礼包数量',
+  `name` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼包名称',
+  `details` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼包详情',
+  `icon` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼包头像',
+  `number` int(10) NOT NULL DEFAULT 0 COMMENT '礼包数量',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '礼包表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_message
 -- ----------------------------
 DROP TABLE IF EXISTS `t_message`;
 CREATE TABLE `t_message`  (
-  `id` int(11) NOT NULL,
-  `message_head` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '消息头',
-  `message_content` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '消息体',
-  `message_date` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '消息日期',
-  `message_status` varchar(5) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '-1' COMMENT '消息状态',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `head` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '消息头',
+  `content` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '消息体',
+  `date` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '发送日期',
+  `status` varchar(5) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '-1' COMMENT '消息状态',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for t_others_login_user
--- ----------------------------
-DROP TABLE IF EXISTS `t_others_login_user`;
-CREATE TABLE `t_others_login_user`  (
-  `id` int(11) NOT NULL,
-  `source` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '第三方来源',
-  `user_id` int(11) NOT NULL DEFAULT -1 COMMENT '用户id',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '消息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_prize
@@ -96,13 +85,24 @@ CREATE TABLE `t_others_login_user`  (
 DROP TABLE IF EXISTS `t_prize`;
 CREATE TABLE `t_prize`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `prize_name` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '奖品名称',
-  `prize_date` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '获奖时间',
-  `prize_expiry_date` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '有效期',
-  `prize_status` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '奖品状态',
-  `prize_details` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '奖品详情',
+  `name` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼品名称',
+  `date` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '礼品日期',
+  `expiry_date` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '到期日',
+  `status` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼品状态',
+  `details` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼品详情',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '礼品表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_thirdparty
+-- ----------------------------
+DROP TABLE IF EXISTS `t_thirdparty`;
+CREATE TABLE `t_thirdparty`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `source` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '登陆来源',
+  `user_id` int(11) NOT NULL DEFAULT -1 COMMENT '用户id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '第三方登陆表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_user
@@ -113,13 +113,13 @@ CREATE TABLE `t_user`  (
   `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '用户名',
   `password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '密码',
   `sex` int(5) NOT NULL COMMENT '性别',
-  `phone_number` bigint(20) NOT NULL COMMENT '电话号码',
+  `phone` bigint(20) NOT NULL COMMENT '手机号',
   `email` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邮箱',
-  `user_icon` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户头像',
-  `user_level` int(10) NOT NULL DEFAULT 1 COMMENT '用户等级',
-  `user_property` int(10) NOT NULL DEFAULT 0 COMMENT '用户资产',
-  `user_ep` int(10) NOT NULL DEFAULT 0 COMMENT '经验值',
+  `icon` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '头像',
+  `level` int(10) NOT NULL DEFAULT 1 COMMENT '等级',
+  `property` int(10) NOT NULL DEFAULT 0 COMMENT '资产',
+  `experience` int(10) NOT NULL DEFAULT 0 COMMENT '经验',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1000000 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
