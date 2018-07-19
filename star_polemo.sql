8eb5a2556cad590338bc2e13c6a4e02834aec451
zhouzj <2473047707@qq.com>
2018-07-18 20:47:13 -0700

    修改表设计，新增业务表插入时间，更新时间等字段。

--- star_polemo.sql
+++ star_polemo.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50716
  File Encoding         : 65001
 
- Date: 18/07/2018 10:28:58
+ Date: 19/07/2018 11:45:56
 */
 
 SET NAMES utf8;
@@ -25,7 +25,7 @@ CREATE TABLE `t_activity`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '活动名称',
   `address` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '活动地址',
-  `icon` varchar(25) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '活动头像',
+  `avatar` varchar(25) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '活动头像',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '活动表' ROW_FORMAT = Dynamic;
 
@@ -47,24 +47,35 @@ DROP TABLE IF EXISTS `t_game`;
 CREATE TABLE `t_game`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '游戏名称',
-  `icon` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '游戏头像',
+  `avatar` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '游戏头像',
   `details` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '游戏详情',
   `address` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '游戏地址',
+  `op` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '操作系统',
+  `score` int(11) NOT NULL COMMENT '分数的十倍，例如4.5分，记录45',
+  `insert_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '插入时间',
+  `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10000 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '游戏表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 10006 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '游戏表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_game
+-- ----------------------------
+INSERT INTO `t_game` VALUES (10005, '????', '????', '????', '????', 'ios', 45, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- ----------------------------
 -- Table structure for t_gift
 -- ----------------------------
 DROP TABLE IF EXISTS `t_gift`;
 CREATE TABLE `t_gift`  (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼包名称',
   `details` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼包详情',
-  `icon` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼包头像',
-  `number` int(10) NOT NULL DEFAULT 0 COMMENT '礼包数量',
+  `avatar` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼包头像',
+  `number` int(11) NOT NULL DEFAULT 0 COMMENT '礼包数量',
+  `insert_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '插入时间',
+  `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '礼包表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '礼包表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_message
@@ -75,7 +86,7 @@ CREATE TABLE `t_message`  (
   `head` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '消息头',
   `content` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '消息体',
   `date` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '发送日期',
-  `status` varchar(5) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '-1' COMMENT '消息状态',
+  `status` tinyint(1) NOT NULL DEFAULT -1 COMMENT '消息状态',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '消息表' ROW_FORMAT = Dynamic;
 
@@ -88,10 +99,32 @@ CREATE TABLE `t_prize`  (
   `name` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼品名称',
   `date` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '礼品日期',
   `expiry_date` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '到期日',
-  `status` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼品状态',
+  `status` tinyint(1) NOT NULL DEFAULT '' COMMENT '礼品状态',
   `details` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '礼品详情',
+  `type` tinyint(1) NOT NULL COMMENT '礼品类型',
+  `price` int(11) NOT NULL COMMENT '价格',
+  `insert_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '插入时间',
+  `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '礼品表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '礼品表（商城物品）' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_task
+-- ----------------------------
+DROP TABLE IF EXISTS `t_task`;
+CREATE TABLE `t_task`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '名称',
+  `avatar` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '头像',
+  `status` tinyint(1) NOT NULL DEFAULT -1 COMMENT '任务状态',
+  `user_id` int(11) NOT NULL DEFAULT -1 COMMENT '所属用户id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci COMMENT = '首页任务表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_task
+-- ----------------------------
+INSERT INTO `t_task` VALUES (1, 'task', 'icon', 0, 1);
 
 -- ----------------------------
 -- Table structure for t_thirdparty
@@ -99,7 +132,7 @@ CREATE TABLE `t_prize`  (
 DROP TABLE IF EXISTS `t_thirdparty`;
 CREATE TABLE `t_thirdparty`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `source` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '登陆来源',
+  `source` tinyint(1) NOT NULL DEFAULT '' COMMENT '登陆来源',
   `user_id` int(11) NOT NULL DEFAULT -1 COMMENT '用户id',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '第三方登陆表' ROW_FORMAT = Dynamic;
@@ -112,13 +145,15 @@ CREATE TABLE `t_user`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '用户名',
   `password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '密码',
-  `sex` int(5) NOT NULL COMMENT '性别',
-  `phone` bigint(20) NOT NULL COMMENT '手机号',
-  `email` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邮箱',
-  `icon` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '头像',
-  `level` int(10) NOT NULL DEFAULT 1 COMMENT '等级',
-  `property` int(10) NOT NULL DEFAULT 0 COMMENT '资产',
-  `experience` int(10) NOT NULL DEFAULT 0 COMMENT '经验',
+  `sex` tinyint(1) NOT NULL DEFAULT 3 COMMENT '性别',
+  `phone` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '手机号',
+  `email` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '邮箱',
+  `avatar` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '头像',
+  `level` int(11) NOT NULL DEFAULT 1 COMMENT '等级',
+  `property` int(11) NOT NULL DEFAULT 0 COMMENT '资产',
+  `experience` int(11) NOT NULL DEFAULT 0 COMMENT '经验',
+  `insert_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '插入时间',
+  `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1000000 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
 
